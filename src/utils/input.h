df80af0c24e560415078de5554ec2490b41d835e
Maschell <Maschell@gmx.de>
2022-09-24 22:57:24 +0200

    Add support for Classic Controller, Pro Controller and Wiimote

---
+++ src/utils/input.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+#include <padscore/kpad.h>
+#include <vpad/input.h>
+
+
+uint32_t remapVPADtoWiimote(uint32_t buttons);
+uint32_t remapVPADtoClassic(uint32_t buttons);
+uint32_t remapVPADtoPro(uint32_t buttons);
+
+uint32_t remapWiiMoteButtons(uint32_t buttons);
+uint32_t remapClassicButtons(uint32_t buttons);
