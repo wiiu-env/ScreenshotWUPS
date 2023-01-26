29cc253c348131c218c380fa0401caef307bb7bd
Maschell <Maschell@gmx.de>
2023-01-26 12:46:03 +0100

    Move getting the name of the current application into utils

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <memory>
+#include <string>
 
 extern uint8_t SRGBComponentToRGBTable[];
 extern uint8_t RGBComponentToSRGBTable[];
@@ -12,3 +13,5 @@ inline uint8_t SRGBComponentToRGB(uint8_t ci) {
 inline uint8_t RGBComponentToSRGB(uint8_t ci) {
     return RGBComponentToSRGBTable[ci];
 }
+
+std::string GetSanitizedNameOfCurrentApplication();
