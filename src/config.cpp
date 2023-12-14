6795e12f3719cd63c50616872bfd8c84e5c5f3f0
Maschell <Maschell@gmx.de>
2023-12-14 21:29:11 +0100

    Implement support for WUPS 0.8.0

--- src/config.cpp
+++ src/config.cpp
@@ -6,6 +6,7 @@
 #include "utils/utils.h"
 #include <vpad/input.h>
 #include <wups.h>
+#include <wups/config/WUPSConfigCategory.h>
 #include <wups/config/WUPSConfigItemBoolean.h>
 #include <wups/config/WUPSConfigItemIntegerRange.h>
 #include <wups/config/WUPSConfigItemMultipleValues.h>
@@ -13,76 +14,50 @@
 
 WUPS_USE_STORAGE("screenshot_plugin");
 
+WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle rootHandle);
+
+void ConfigMenuClosedCallback() {
+    WUPSStorageError storageRes;
+    if ((storageRes = WUPSStorageAPI::SaveStorage()) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to close storage %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
+}
+
 void InitConfig() {
+    WUPSConfigAPIOptionsV1 configOptions = {.name = "Screenshot Plugin"};
+    if (WUPSConfigAPI_Init(configOptions, ConfigMenuOpenedCallback, ConfigMenuClosedCallback) != WUPSCONFIG_API_RESULT_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to init config api");
+    }
+
     gButtonCombo = VPAD_BUTTON_TV;
-    // Open storage to read values
-    WUPSStorageError storageRes = WUPS_OpenStorage();
-    if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-        DEBUG_FUNCTION_LINE_ERR("Failed to open storage %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-    } else {
-        // Try to get value from storage
-        if ((storageRes = WUPS_GetBool(nullptr, ENABLED_CONFIG_STRING, &gEnabled)) == WUPS_STORAGE_ERROR_NOT_FOUND) {
-            // Add the value to the storage if it's missing.
-            if (WUPS_StoreBool(nullptr, ENABLED_CONFIG_STRING, gEnabled) != WUPS_STORAGE_ERROR_SUCCESS) {
-                DEBUG_FUNCTION_LINE_ERR("Failed to store value");
-            }
-        } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to get value %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-        }
-        // Try to get value from storage
-        if ((storageRes = WUPS_GetInt(nullptr, FORMAT_CONFIG_STRING, reinterpret_cast<int32_t *>(&gOutputFormat))) == WUPS_STORAGE_ERROR_NOT_FOUND) {
-            // Add the value to the storage if it's missing.
-            if (WUPS_StoreInt(nullptr, FORMAT_CONFIG_STRING, gOutputFormat) != WUPS_STORAGE_ERROR_SUCCESS) {
-                DEBUG_FUNCTION_LINE_ERR("Failed to store value");
-            }
-        } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to get value %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-        }
 
-        // Try to get value from storage
-        if ((storageRes = WUPS_GetInt(nullptr, QUALITY_CONFIG_STRING, reinterpret_cast<int32_t *>(&gQuality))) == WUPS_STORAGE_ERROR_NOT_FOUND) {
-            // Add the value to the storage if it's missing.
-            if (WUPS_StoreInt(nullptr, QUALITY_CONFIG_STRING, (int32_t) gQuality) != WUPS_STORAGE_ERROR_SUCCESS) {
-                DEBUG_FUNCTION_LINE_ERR("Failed to store value");
-            }
-        } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to get value %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-        }
+    WUPSStorageError storageRes;
+    if ((storageRes = WUPSStorageAPI::GetOrStoreDefault(ENABLED_CONFIG_STRING, gEnabled, ENABLED_CONFIG_DEFAULT)) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to GetOrStoreDefault value %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
 
-        // Try to get value from storage
-        if ((storageRes = WUPS_GetInt(nullptr, SCREEN_CONFIG_STRING, reinterpret_cast<int32_t *>(&gImageSource))) == WUPS_STORAGE_ERROR_NOT_FOUND) {
-            // Add the value to the storage if it's missing.
-            if (WUPS_StoreInt(nullptr, SCREEN_CONFIG_STRING, (int32_t) gImageSource) != WUPS_STORAGE_ERROR_SUCCESS) {
-                DEBUG_FUNCTION_LINE_ERR("Failed to store value");
-            }
-        } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to get value %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-        }
+    if ((storageRes = WUPSStorageAPI::GetOrStoreDefault(FORMAT_CONFIG_STRING, gOutputFormat, FORMAT_CONFIG_DEFAULT)) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to GetOrStoreDefault value %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
 
-        // Try to get value from storage
-        if ((storageRes = WUPS_GetInt(nullptr, BUTTON_COMBO_CONFIG_STRING, reinterpret_cast<int32_t *>(&gButtonCombo))) == WUPS_STORAGE_ERROR_NOT_FOUND) {
-            // Add the value to the storage if it's missing.
-            if (WUPS_StoreInt(nullptr, BUTTON_COMBO_CONFIG_STRING, (int32_t) gButtonCombo) != WUPS_STORAGE_ERROR_SUCCESS) {
-                DEBUG_FUNCTION_LINE_ERR("Failed to store value");
-            }
-        } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to get value %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-        }
+    if ((storageRes = WUPSStorageAPI::GetOrStoreDefault(QUALITY_CONFIG_STRING, gQuality, QUALITY_CONFIG_DEFAULT)) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to GetOrStoreDefault value %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
 
-        // Try to get value from storage
-        if ((storageRes = WUPS_GetBool(nullptr, RESERVED_BIT_USAGE_CONFIG_STRING, &gReservedBitUsage)) == WUPS_STORAGE_ERROR_NOT_FOUND) {
-            // Add the value to the storage if it's missing.
-            if (WUPS_StoreBool(nullptr, RESERVED_BIT_USAGE_CONFIG_STRING, gReservedBitUsage) != WUPS_STORAGE_ERROR_SUCCESS) {
-                DEBUG_FUNCTION_LINE_ERR("Failed to store value");
-            }
-        } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to get value %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
-        }
+    if ((storageRes = WUPSStorageAPI::GetOrStoreDefault(SCREEN_CONFIG_STRING, gImageSource, SCREEN_CONFIG_DEFAULT)) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to GetOrStoreDefault value %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
 
-        // Close storage
-        if (WUPS_CloseStorage() != WUPS_STORAGE_ERROR_SUCCESS) {
-            DEBUG_FUNCTION_LINE_ERR("Failed to close storage");
-        }
+    if ((storageRes = WUPSStorageAPI::GetOrStoreDefault<uint32_t>(BUTTON_COMBO_CONFIG_STRING, gButtonCombo, BUTTON_COMBO_CONFIG_DEFAULT)) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to GetOrStoreDefault value %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
+
+    if ((storageRes = WUPSStorageAPI::GetOrStoreDefault(RESERVED_BIT_USAGE_CONFIG_STRING, gReservedBitUsage, RESERVED_BIT_USAGE_CONFIG_DEFAULT)) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to GetOrStoreDefault value %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
+    }
+
+    if ((storageRes = WUPSStorageAPI::SaveStorage()) != WUPS_STORAGE_ERROR_SUCCESS) {
+        DEBUG_FUNCTION_LINE_ERR("Failed to save storage %s (%d)", WUPSStorageAPI::GetStatusStr(storageRes).data(), storageRes);
     }
 
     if (gButtonCombo & VPAD_BUTTON_TV) {
@@ -104,32 +79,38 @@ void InitConfig() {
 }
 
 void multipleValueItemCallback(ConfigItemMultipleValues *item, uint32_t newValue) {
-    if (item && item->configId) {
+    if (item && item->identifier) {
         DEBUG_FUNCTION_LINE("New value in %s changed: %d", item->configId, newValue);
-        if (std::string_view(item->configId) == FORMAT_CONFIG_STRING) {
+        if (std::string_view(item->identifier) == FORMAT_CONFIG_STRING) {
             gOutputFormat = (ImageOutputFormatEnum) newValue;
 
             if (gOutputFormat >= 3) {
                 gOutputFormat = IMAGE_OUTPUT_FORMAT_JPEG;
             }
 
-            WUPS_StoreInt(nullptr, item->configId, (int32_t) newValue);
-        } else if (std::string_view(item->configId) == SCREEN_CONFIG_STRING) {
+            WUPSStorageError err;
+            if ((err = WUPSStorageAPI::Store(item->identifier, gOutputFormat)) != WUPS_STORAGE_ERROR_SUCCESS) {
+                DEBUG_FUNCTION_LINE_ERR("Failed to store item %s (newValue: %d): %s", item->identifier, gOutputFormat, WUPSStorageAPI::GetStatusStr(err).data());
+            }
+        } else if (std::string_view(item->identifier) == SCREEN_CONFIG_STRING) {
             gImageSource = (ImageSourceEnum) newValue;
 
             if (gImageSource >= 3) {
                 gImageSource = IMAGE_SOURCE_TV_AND_DRC;
             }
 
-            WUPS_StoreInt(nullptr, item->configId, (int32_t) newValue);
+            WUPSStorageError err;
+            if ((err = WUPSStorageAPI::Store(item->identifier, gImageSource)) != WUPS_STORAGE_ERROR_SUCCESS) {
+                DEBUG_FUNCTION_LINE_ERR("Failed to store item %s (newValue: %d): %s", item->identifier, gImageSource, WUPSStorageAPI::GetStatusStr(err).data());
+            }
         }
     }
 }
 
 void integerRangeItemCallback(ConfigItemIntegerRange *item, int32_t newValue) {
-    if (item && item->configId) {
+    if (item && item->identifier) {
         DEBUG_FUNCTION_LINE("New value in %s changed: %d", item->configId, newValue);
-        if (std::string_view(item->configId) == QUALITY_CONFIG_STRING) {
+        if (std::string_view(item->identifier) == QUALITY_CONFIG_STRING) {
             gQuality = (ImageOutputFormatEnum) newValue;
 
             if (gQuality < 10) {
@@ -138,31 +119,40 @@ void integerRangeItemCallback(ConfigItemIntegerRange *item, int32_t newValue) {
                 gQuality = 100;
             }
 
-            WUPS_StoreInt(nullptr, item->configId, (int32_t) gQuality);
+            WUPSStorageError err;
+            if ((err = WUPSStorageAPI::Store(item->identifier, gQuality)) != WUPS_STORAGE_ERROR_SUCCESS) {
+                DEBUG_FUNCTION_LINE_ERR("Failed to store item %s (newValue: %d): %s", item->identifier, gQuality, WUPSStorageAPI::GetStatusStr(err).data());
+            }
         }
     }
 }
 
 void boolItemCallback(ConfigItemBoolean *item, bool newValue) {
-    if (item && item->configId) {
+    if (item && item->identifier) {
         DEBUG_FUNCTION_LINE("New value in %s changed: %d", item->configId, newValue);
-        if (std::string_view(item->configId) == ENABLED_CONFIG_STRING) {
+        if (std::string_view(item->identifier) == ENABLED_CONFIG_STRING) {
             gEnabled = newValue;
             if (gEnabled) {
                 InitNotificationModule();
             }
-            WUPS_StoreBool(nullptr, item->configId, gEnabled);
-        } else if (std::string_view(item->configId) == RESERVED_BIT_USAGE_CONFIG_STRING) {
+            WUPSStorageError err;
+            if ((err = WUPSStorageAPI::Store(item->identifier, gEnabled)) != WUPS_STORAGE_ERROR_SUCCESS) {
+                DEBUG_FUNCTION_LINE_ERR("Failed to store item %s (newValue: %d): %s", item->identifier, gEnabled, WUPSStorageAPI::GetStatusStr(err).data());
+            }
+        } else if (std::string_view(item->identifier) == RESERVED_BIT_USAGE_CONFIG_STRING) {
             gReservedBitUsage = newValue;
-            WUPS_StoreBool(nullptr, item->configId, gReservedBitUsage);
+            WUPSStorageError err;
+            if ((err = WUPSStorageAPI::Store(item->identifier, gReservedBitUsage)) != WUPS_STORAGE_ERROR_SUCCESS) {
+                DEBUG_FUNCTION_LINE_ERR("Failed to store item %s (newValue: %d): %s", item->identifier, gReservedBitUsage, WUPSStorageAPI::GetStatusStr(err).data());
+            }
         }
     }
 }
 
 void buttonComboItemChanged(ConfigItemButtonCombo *item, uint32_t newValue) {
-    if (item && item->configId) {
-        DEBUG_FUNCTION_LINE("New value in %s changed: %d", item->configId, newValue);
-        if (std::string_view(item->configId) == BUTTON_COMBO_CONFIG_STRING) {
+    if (item && item->identifier) {
+        DEBUG_FUNCTION_LINE("New value in %s changed: %08X", item->configId, newValue);
+        if (std::string_view(item->identifier) == BUTTON_COMBO_CONFIG_STRING) {
             gButtonCombo = newValue;
             if (gButtonCombo & VPAD_BUTTON_TV) {
                 DEBUG_FUNCTION_LINE("Block TV Menu");
@@ -171,85 +161,65 @@ void buttonComboItemChanged(ConfigItemButtonCombo *item, uint32_t newValue) {
                 DEBUG_FUNCTION_LINE("Unblock TV Menu");
                 VPADSetTVMenuInvalid(VPAD_CHAN_0, false);
             }
-            WUPS_StoreInt(nullptr, item->configId, (int32_t) gButtonCombo);
+            WUPSStorageError err;
+            if ((err = WUPSStorageAPI::Store(item->identifier, gButtonCombo)) != WUPS_STORAGE_ERROR_SUCCESS) {
+                DEBUG_FUNCTION_LINE_ERR("Failed to store item %s (newValue: %08X): %s", item->identifier, gButtonCombo, WUPSStorageAPI::GetStatusStr(err).data());
+            }
         }
     }
 }
 
-WUPS_GET_CONFIG() {
-    // We open the storage, so we can persist the configuration the user did.
-    if (WUPS_OpenStorage() != WUPS_STORAGE_ERROR_SUCCESS) {
-        DEBUG_FUNCTION_LINE_ERR("Failed to open storage");
-        return 0;
+WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle rootHandle) {
+    try {
+        WUPSConfigCategory root = WUPSConfigCategory(rootHandle);
+
+        root.add(WUPSConfigItemBoolean::Create(ENABLED_CONFIG_STRING,
+                                               "Enabled",
+                                               ENABLED_CONFIG_DEFAULT, gEnabled,
+                                               &boolItemCallback));
+
+        root.add(WUPSConfigItemButtonCombo::Create(BUTTON_COMBO_CONFIG_STRING,
+                                                   "Button combo",
+                                                   BUTTON_COMBO_CONFIG_DEFAULT, gButtonCombo,
+                                                   &buttonComboItemChanged));
+
+        constexpr WUPSConfigItemMultipleValues::ValuePair possibleScreenValues[] = {
+                {IMAGE_SOURCE_TV_AND_DRC, "TV & GamePad"},
+                {IMAGE_SOURCE_TV, "TV only"},
+                {IMAGE_SOURCE_DRC, "GamePad only"},
+        };
+
+        root.add(WUPSConfigItemMultipleValues::CreateFromValue(SCREEN_CONFIG_STRING,
+                                                               "Screen",
+                                                               SCREEN_CONFIG_DEFAULT, gImageSource,
+                                                               possibleScreenValues,
+                                                               &multipleValueItemCallback));
+
+        constexpr WUPSConfigItemMultipleValues::ValuePair possibleFormatValues[] = {
+                {IMAGE_OUTPUT_FORMAT_JPEG, "JPEG"},
+                {IMAGE_OUTPUT_FORMAT_PNG, "PNG"},
+                {IMAGE_OUTPUT_FORMAT_BMP, "BMP"}};
+
+        root.add(WUPSConfigItemMultipleValues::CreateFromValue(FORMAT_CONFIG_STRING,
+                                                               "Output format",
+                                                               FORMAT_CONFIG_DEFAULT, gOutputFormat,
+                                                               possibleFormatValues,
+                                                               &multipleValueItemCallback));
+
+        root.add(WUPSConfigItemIntegerRange::Create(FORMAT_CONFIG_STRING,
+                                                    "JPEG quality",
+                                                    FORMAT_CONFIG_DEFAULT, gQuality,
+                                                    10, 100,
+                                                    &integerRangeItemCallback));
+        root.add(WUPSConfigItemBoolean::Create(RESERVED_BIT_USAGE_CONFIG_STRING,
+                                               "Check ReservedBit for taking screenshots",
+                                               RESERVED_BIT_USAGE_CONFIG_DEFAULT, gReservedBitUsage,
+                                               &boolItemCallback));
+
+    } catch (std::exception &e) {
+        OSReport("Exception T_T : %s\n", e.what());
+        return WUPSCONFIG_API_CALLBACK_RESULT_ERROR;
     }
 
-    WUPSConfigHandle config;
-    WUPSConfig_CreateHandled(&config, "Screenshot plugin");
-
-    WUPSConfigCategoryHandle setting;
-    WUPSConfig_AddCategoryByNameHandled(config, "Settings", &setting);
-
-    WUPSConfigItemBoolean_AddToCategoryHandled(config, setting, ENABLED_CONFIG_STRING, "Enabled", gEnabled, &boolItemCallback);
-
-    WUPSConfigItemButtonCombo_AddToCategoryHandled(config, setting, BUTTON_COMBO_CONFIG_STRING, "Button combo", gButtonCombo, &buttonComboItemChanged);
-
-    ConfigItemMultipleValuesPair source[3];
-    source[0].value     = IMAGE_SOURCE_TV_AND_DRC;
-    source[0].valueName = (char *) "TV & GamePad";
-
-    source[1].value     = IMAGE_SOURCE_TV;
-    source[1].valueName = (char *) "TV only";
-
-    source[2].value     = IMAGE_SOURCE_DRC;
-    source[2].valueName = (char *) "GamePad only";
-
-    uint32_t defaultIndex = 0;
-    uint32_t curIndex     = 0;
-    for (auto &cur : source) {
-        if (cur.value == gImageSource) {
-            defaultIndex = curIndex;
-            break;
-        }
-        curIndex++;
-    }
-
-    WUPSConfigItemMultipleValues_AddToCategoryHandled(config, setting, SCREEN_CONFIG_STRING, "Screen", defaultIndex, source,
-                                                      sizeof(source) / sizeof(source[0]), &multipleValueItemCallback);
-
-    ConfigItemMultipleValuesPair fileFormat[3];
-    fileFormat[0].value     = IMAGE_OUTPUT_FORMAT_JPEG;
-    fileFormat[0].valueName = (char *) "JPEG";
-
-    fileFormat[1].value     = IMAGE_OUTPUT_FORMAT_PNG;
-    fileFormat[1].valueName = (char *) "PNG";
-
-    fileFormat[2].value     = IMAGE_OUTPUT_FORMAT_BMP;
-    fileFormat[2].valueName = (char *) "BMP";
-
-    defaultIndex = 0;
-    curIndex     = 0;
-    for (auto &cur : fileFormat) {
-        if (cur.value == gOutputFormat) {
-            defaultIndex = curIndex;
-            break;
-        }
-        curIndex++;
-    }
-
-    WUPSConfigItemMultipleValues_AddToCategoryHandled(config, setting, FORMAT_CONFIG_STRING, "Output format", defaultIndex, fileFormat,
-                                                      sizeof(fileFormat) / sizeof(fileFormat[0]), &multipleValueItemCallback);
-
-
-    WUPSConfigItemIntegerRange_AddToCategoryHandled(config, setting, QUALITY_CONFIG_STRING, "JPEG quality", gQuality, 10, 100, &integerRangeItemCallback);
-
-    WUPSConfigItemBoolean_AddToCategoryHandled(config, setting, RESERVED_BIT_USAGE_CONFIG_STRING, "Check ReservedBit for taking screenshots", gReservedBitUsage, &boolItemCallback);
-
-    return config;
-}
-
-WUPS_CONFIG_CLOSED() {
-    // Save all changes
-    if (WUPS_CloseStorage() != WUPS_STORAGE_ERROR_SUCCESS) {
-        DEBUG_FUNCTION_LINE_ERR("Failed to close storage");
-    }
+    return WUPSCONFIG_API_CALLBACK_RESULT_SUCCESS;
 }
